1d3da2da85c9379213b8b6fef75cd652b7c2bdf7
Arjun Lamba <arjunlam@bu.edu>
2016-04-30 11:49:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var logger      = require('morgan');
 var bodyParser  = require('body-parser');
 var cookieParser= require('cookie-parser');
 var passport    = require('passport');
-var config      = require('./config/passport');
+var LocalStrategy = require('passport-local').Strategy;
+var session     = require('expression-session');
+var spatialUser = require('./models/user.js');
 
 
 //HELLOOOOOOOOOOOOO
@@ -37,6 +39,66 @@ app.use(express.session({secrect: 'secret'}));
 app.use(passport.initialize()); //startup up passport
 app.use(passport.session()); //startup session
 
+//for sessions but we are only doing log in and not a session (since we are not doing other requests)
+passport.serializeUser(function(user,done) {
+    done(null,user.id);
+});
+
+passport.deserializeUser(function(id,done) {
+    spatial-user.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+
+
+//CONFIGURE PASSPORT LOCAL STRATEGY
+passport.use('local-login', new LocalStrategy(
+    function(username, password, done) {
+        spatial-user.findOne({'username': username}, function(err,user) {
+            if (err) { return done(err); }
+            if (!user) {
+                return done(null, false);
+            }
+            if (!user.validPassword(password)) { //or you can do  user.password != password
+                return done(null, false);
+            }
+            
+            return done(null, user);
+        });
+    }));
+
+//CONFIGURE PASSPORT SIGNUP STRATEGY
+passport.use('local-signup', new LocalStrategy(
+    function(username, password, done) {
+        process.nextTick(function() {
+            spatial-user.findOne({ 'username' : username }, function(err,user) { //checking to see if the username already exists
+                if (err) //if there are any errors then return error
+                    return done(err);
+                if (user) { //check if user already exists
+                    return done(null, false);
+                } else {
+                    //create new user
+                    var newUser = new spatial-user();
+                    
+                    //set user credentials
+                    newUser.username = username;
+                    newUser.password = newUser.generateHash(password);
+                    
+                    //save new user
+                    newUser.save(function(err) {
+                        if (err)
+                            throw err;
+                        return done(null, newUser);
+                    });
+                }
+            });
+        });
+    }));
+
+
+
+
 
 // load in routes
 app.use('/', users);
@@ -45,7 +107,25 @@ app.use('/', index);
 app.use('/login', login);
 app.use('/signup', signup);
 
-require('./routes/passport')(app, passport);
+
+
+//PASSPORT.JS ROUTES
+app.get('/index', function(req,res) {
+    res.render('index');
+});
+
+app.get('/login', function(req,res) {
+    res.render('login');
+});
+
+app.get('/signup', function(res,req) {
+    res.render('signup');
+});
+
+app.get('/logout', function(req,res) {
+    req.logout();
+    res.redirect('/index');
+});
 
 //post to process login form
 app.post('/login', passport.authenticate('local-login', {successRedirect: '/index', failureRedirect: '/login'}));
