9cdc7e5050e01165f2ee3f6192ecb5800141f5cd
Arjun Lamba <arjunlam@bu.edu>
2016-04-30 11:15:15 -0400

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,5 +1,33 @@
 var mongoose    = require('mongoose');
 var Schema      = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');  //to encrypt password
+
+// User Schema = define structure of the data inside a collection
+var UserSchema = new Schema({
+    username: String,
+    password: String
+}, {
+    collection: 'spatial-user'
+});
+
+
+//HASHING THE PASSWORD WITHIN OUR USER MODEL BEFORE IT IS SAVED TO DB
+//generate hash
+UserSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+});
+
+//check that password is valid
+UserSchema.method.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+});
+
+
+module.exports = mongoose.model('spatial-user', UserSchema);
+
+
+/*var mongoose    = require('mongoose');
+var Schema      = mongoose.Schema;
 
 // User Schema
 var UserSchema = new Schema({
@@ -21,3 +49,4 @@ UserSchema.pre('save', function(next){
 });
 
 module.exports = mongoose.model('spatial-user', UserSchema);
+*/
